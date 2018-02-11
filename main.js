5208b37ea25a6be51ece1fae8ce886ba42983d99
Bilal Waseem <bilalwc42@gmail.com>
2018-02-11 22:32:29 +0500

    added proof of concept

--- main.js
+++ main.js
@@ -7,16 +7,27 @@ class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
     
     calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+    
+    mineBlock(difficulty) {
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        
+        console.log('Block mined ' + this.hash);
     }
 }
 
 class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4;
     }
     
     createGenesisBlock() {
@@ -27,9 +38,10 @@ class Blockchain {
         return this.chain[this.chain.length -1];
     }
     
+    
     addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash; //sets the newblock's previous hash to the last block's hash
-        newBlock.hash = newBlock.calculateHash(); //calculates the hash of the new block
+        newBlock.mineBlock(this.difficulty); //mines the hash of the new block till it gets a hash with first 4 diffulty level
         this.chain.push(newBlock); //push the newblock to the chain
     }
     
@@ -53,7 +65,11 @@ class Blockchain {
 }
 
 let myCoin = new Blockchain();
+
+console.log('Mining 1st block...');
 myCoin.addBlock(new Block(1, '05/01/2018', { amount: 200 }));
+
+console.log('Mining 2nd block...')
 myCoin.addBlock(new Block(2, '25/01/2018', { amount: 400 }));
 
 console.log(JSON.stringify(myCoin, null, 4));
